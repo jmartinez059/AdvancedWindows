ce5d72c769ac87e9fde0b6f62322d2081e99be00
Jason Martinez <jmartinez059@yahoo.com>
2019-05-23 02:50:29 -0500

    created modal to display service info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { ServicesComponent } from './components/services/services.component';
 import { AppointmentComponent } from './components/appointment/appointment.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { AboutComponent } from './components/about/about.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ServiceInfoModalComponent } from './components/modal/service-info-modal/service-info-modal.component';
+import { MatDialogModule, MatInputModule, MatButtonModule, MatFormFieldModule, MAT_DIALOG_DATA } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
@@ -18,12 +26,29 @@ import { AboutComponent } from './components/about/about.component';
     AppointmentComponent,
     ContactComponent,
     AboutComponent,
+    ModalComponent,
+    ServiceInfoModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide: MAT_DIALOG_DATA, useValue: {hasBackdrop: true}}
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  entryComponents: [
+    ServiceInfoModalComponent,
+    ModalComponent
+  ]
 })
 export class AppModule { }
